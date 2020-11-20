f06e96d13ab5d6a3cf006f006717d0d579880ca2
Nikhil-Rane108 <71148109+Nikhil-Rane108@users.noreply.github.com>
2020-11-20 23:39:09 +0530

    Update Largest no.java

--- Largest no.java
+++ Largest no.java
@@ -14,17 +14,24 @@ public class Largest
 
       System.out.println("Enter Third Number: ");
       num3 = in1.nextInt();
-      if((num1>num2) && (num1>num3))
-      {
-          System.out.println("The largest number is: "+num1);
-      }
-      else if((num2>num1) && (num2>num3))
-      {
-          System.out.println("The largest number is: "+num2);
-      }    
-      else
-      {
-          System.out.println("The largest number is: "+num3);
-      }
+      if(num1>num2)
+          {
+            if(num1>num3)
+            {
+            System.out.println("The largest no. is "+num1);
+            }
+            else
+            {
+            System.out.println("The largest no. is "+num3);
+            }
+          }
+        else if(num2>num3)
+        {
+            System.out.println("The largest no. is "+num2);
+        }
+        else
+        {
+            System.out.println("The largest no. is "+num3);
+        }
     }
 }    
