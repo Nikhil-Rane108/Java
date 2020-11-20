154e1811076c0e5a3d96d49b29630f3377a7cfa4
Nikhil-Rane108 <71148109+Nikhil-Rane108@users.noreply.github.com>
2020-11-20 23:09:05 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,9 +1,22 @@
-public class addition
+import java.util.Scanner;
+public class Addition 
 {
-   public static void main(String args[]) 
-   { 
-      int x=25,y=15,add;
-      add=x+y;
-      System.out.println("Sum of these numbers: "+add);
-   }
+    public static void main(String args[])
+    {
+      Scanner scan= new Scanner(System.in);
+       try
+       {
+            int add; 
+            System.out.println("Enter first number:");
+            int x=scan.nextInt();
+            System.out.println("Enter second number:");
+            int y=scan.nextInt();
+            add=x+y;
+            System.out.println("Addition of number is "+add);
+       }
+       catch(Exception e)
+       { 
+           System.out.println("Error: "+e);
+       }
+    }
 }
