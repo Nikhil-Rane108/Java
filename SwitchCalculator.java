693f6b85eda1bca3eed43d9e2cad521d78aae98a
Nikhil-Rane108 <71148109+Nikhil-Rane108@users.noreply.github.com>
2020-11-23 23:58:54 +0530

    Add files via upload

---
+++ SwitchCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Switch_Calculator
+{
+
+  public static void main(String[] args) {
+
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Enter two numbers: ");
+
+    double num1 = reader.nextDouble();
+    double num2 = reader.nextDouble();
+
+    System.out.print("Enter an operator (+, -, *, /): ");
+    char operator = reader.next().charAt(0);
+
+    double result;
+
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+
+      case '-':
+        result = num1 - num2;
+        break;
+
+      case '*':
+        result = num1 * num2;
+        break;
+
+      case '/':
+        result = num1 / num2;
+        break;
+
+      default:
+        System.out.printf("You have entered wrong operator");
+        return;
+    }
+
+    System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+  }
+}
